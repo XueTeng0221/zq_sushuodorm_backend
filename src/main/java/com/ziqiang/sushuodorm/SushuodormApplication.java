899d81b330a3cf2d86416ca107ef3861460e26e6
XueTeng0221 <xt_bryan@qq.com>
2025-01-23 17:45:05 +0800

    first commit

---
+++ src/main/java/com/ziqiang/sushuodorm/SushuodormApplication.java
@@ -0,0 +1,13 @@
+package com.ziqiang.sushuodorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushuodormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushuodormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ziqiang/sushuodorm/SushuodormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziqiang.sushuodorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushuodormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
