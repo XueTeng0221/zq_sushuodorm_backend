535fff54bcc208b72bf0ea52b5acbbfb475267e3
XueTeng0221 <xt_bryan@qq.com>
2025-01-25 21:36:39 +0800

    chore

--- src/main/java/com/ziqiang/sushuodorm/mapper/LikeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ziqiang.sushuodorm.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ziqiang.sushuodorm.entity.item.LikeItem;
-
-public interface LikeMapper extends BaseMapper<LikeItem> {
-}
--- src/main/java/com/ziqiang/sushuodorm/services/LikeService.java
+++
@@ -1,7 +0,0 @@
-package com.ziqiang.sushuodorm.services;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ziqiang.sushuodorm.entity.item.LikeItem;
-
-public interface LikeService extends IService<LikeItem> {
-}
