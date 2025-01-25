96d3e4a38389cd620a679139e1085d84693ec0fd
XueTeng0221 <xt_bryan@qq.com>
2025-01-25 21:52:55 +0800

    add(msg): 添加通知业务

--- src/main/java/com/ziqiang/sushuodorm/services/impl/MsgServiceImpl.java
+++ src/main/java/com/ziqiang/sushuodorm/services/impl/MsgServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ziqiang.sushuodorm.services.impl;
 
+import com.ziqiang.sushuodorm.mapper.UserMapper;
 import com.ziqiang.sushuodorm.services.MsgService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MsgServiceImpl implements MsgService {
+    @Autowired
+    private UserMapper userMapper;
 }
