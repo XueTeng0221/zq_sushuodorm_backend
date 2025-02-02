683dbf62f3696ddfe7b4f7b38507e27c2293a014
XueTeng0221 <xt_bryan@qq.com>
2025-02-02 23:40:18 +0800

    update: 添加查询文档

--- src/main/java/com/ziqiang/sushuodorm/daos/PostEsDao.java
+++ src/main/java/com/ziqiang/sushuodorm/daos/PostEsDao.java
@@ -2,9 +2,15 @@ package com.ziqiang.sushuodorm.daos;
 
 import com.ziqiang.sushuodorm.entity.dto.post.PostEsDTO;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
     List<PostEsDTO> findByUserId(Long userId);
+
+    List<PostEsDTO> findByTitle(String title);
+
+    List<PostEsDTO> findByTitleOrContent(String title, String content);
 }
